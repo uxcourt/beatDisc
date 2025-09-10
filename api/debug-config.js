d4fc52196a3fa61b612e288e8201eabd00dbd1d8
uxcourt <uxcourt@gmail.com>
2025-09-09 17:15:02 -0700

    Create debug-config.js

---
+++ api/debug-config.js
@@ -0,0 +1,10 @@
+// /api/debug-config.js
+module.exports = async (req, res) => {
+  res.status(200).json({
+    supabaseUrlPresent: !!process.env.SUPABASE_URL,
+    serviceRolePresent: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    runtime: 'node',
+    host: req.headers.host
+  });
+};
+module.exports.config = { runtime: 'nodejs20.x' }; // force Node runtime
